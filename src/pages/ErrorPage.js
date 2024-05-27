fd99733d7395ca7a2450ad5e7bd4ce708892e70e
Guang <jouyang19@proton.me>
2024-05-27 00:24:56 -0400

    complete lab

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,16 @@
+import { useRouteError } from "react-router-dom";
+import NavBar from "../components/NavBar";
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <h1>Oops! Looks like something went wrong.</h1>
+      </main>
+    </>
+  );
+}
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,16 +1,39 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import { useParams } from "react-router-dom";
 
 function Movie() {
+  const [movie, setMovie] = useState({});
+  const params = useParams();
+  const movieID = params.id;
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${movieID}`)
+      .then((r) => r.json())
+      .then((movie) => {
+        setMovie(movie);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const genres = movie.genres;
+
+  console.log(genres);
+
   return (
     <>
       <header>
         {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
         {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+        <p>{movie.time} mins</p>
+        {genres && genres.map((genre) => <span key={genre}>{genre}</span>)}
       </main>
     </>
   );
-};
+}
 
 export default Movie;
